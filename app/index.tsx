8bdb9efcd8493f9b1784fe9252b4fe5d937a00ce
matt-fonto <mateus.fonto@gmail.com>
2024-12-05 14:18:33 +0100

    feat: base structure nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -4,26 +4,13 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Hey</Text>
+    <View className="flex-1 bg-[#fff] items-center justify-center">
+      <Text className="text-3xl">Hey</Text>
 
       <StatusBar style="auto" />
-      <Link href="/profile" style={styles.link}>
-        Go To Profile
+      <Link href="/profile" className="bg-blue-400 px-2 py-1 rounded-md mt-10">
+        Go auehaueh Profile
       </Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  link: {
-    color: "blue",
-    marginTop: 10,
-  },
-});
